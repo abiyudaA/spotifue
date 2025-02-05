868c4e718646578e0c2121e6c222642e8483d12e
calvingeofanny <geofannywewe@gmail.com>
2025-02-05 21:48:39 +0700

    all bcrypt

--- models/song.js
+++ models/song.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Song.belongsTo(models.User)
+      Song.belongsTo(models.Genre)
     }
   }
   Song.init({
