7ef3d0ef3bf67139bdcc60027818d71a51fc4e3b
abiyudaA <yudaabiabdulhaq@gmail.com>
2025-02-05 21:16:44 +0700

    feat: router dah ready

--- migrations/20250205090413-create-song.js
+++ migrations/20250205090413-create-song.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "Users",
+          model: "Genres",
           key: "id",
         },
         onUpdate: "cascade",
