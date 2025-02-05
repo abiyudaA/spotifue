8d7ab7c0c41914bec504ba8d1e8ddd0d9f7d5eaa
abiyudaA <yudaabiabdulhaq@gmail.com>
2025-02-05 18:06:33 +0700

    define association genre song

--- models/song.js
+++ models/song.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Song.belongsTo(models.User)
+      Song.belongsTo(models.Genre)
+
     }
   }
   Song.init({
