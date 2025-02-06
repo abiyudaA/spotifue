2235bf4fb98a3d8f818cdeb1d2f8adb4f5843c67
calvingeofanny <geofannywewe@gmail.com>
2025-02-06 20:45:12 +0700

    alldone

--- routers/index.js
+++ routers/index.js
@@ -1,27 +1,24 @@
-const router = require('express').Router()
-const genresRoute = require ('./genresRoute')
-const songsRoute = require ('./songsRoute')
-const profilesRoute = require('./profilesRoute')
-const usersRoute = require ('./usersRoute')
-const userController = require ('../controllers/userController')
-const isAuthenticated = require('../middlewares/authentication')
-
+const router = require("express").Router();
+const genresRoute = require("./genresRoute");
+const songsRoute = require("./songsRoute");
+const profilesRoute = require("./profilesRoute");
+const usersRoute = require("./usersRoute");
+const userController = require("../controllers/userController");
+const { isAuthenticated } = require("../middlewares/authentication");
 
 //register
-router.get('/register', userController.userRegister)
-router.post('/register', userController.saveUserRegister)
+router.get("/register", userController.userRegister);
+router.post("/register", userController.saveUserRegister);
 //login
-router.get('/login', userController.userLogin)
-router.post('/login', userController.loggedIn)
+router.get("/login", userController.userLogin);
+router.post("/login", userController.loggedIn);
 //middleware
-router.use(isAuthenticated)
+router.use(isAuthenticated);
 
+router.get("/logout", userController.logout)
 
 //songs
-router.use('/home', songsRoute)
-
+router.use("/home", songsRoute);
+router.use("/profile", profilesRoute);
 
 module.exports = router;
-
-
-
